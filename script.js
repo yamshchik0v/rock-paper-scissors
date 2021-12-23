8b0683c7b722607905e0485b1cdb20cf7f112908
Aleksandr Yamshchikov <sashayamschik0v@gmail.com>
2021-12-23 23:05:00 +0300

    Final version of the main game

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ class RockPaperScissors {
 
       this.variants.forEach(variant => {
          variant.addEventListener('click', (e) => {
-            const playerChoice = e.target.id
+            const playerChoice = e.target
             this.play(playerChoice)
          })
       })
    }
    
    play(variant) {
-      const computerVariant = this.computer().id;
-      console.log(`my choice ${variant} | computer's: ${computerVariant}`);
-      this.render.createStory(variant, player)
-      this.render.createStory(computerVariant, computer)
+      const computerVariant = this.computer();
+      let playerVariantIndex = this.variants.indexOf( variant );
+      let computerVariandIndex = this.variants.indexOf( computerVariant )
+      console.log(`my index: ${this.variants.indexOf( variant )} | computer's index ${this.variants.indexOf(computerVariant)} `);
+      
+      this.render.createStory( variant.id, player )
+      this.render.createStory( computerVariant.id, computer )
+
+      const result = this.winner( playerVariantIndex, computerVariandIndex );
+      
+      this.showResult( result )
    }
 
    computer() {
@@ -25,6 +32,28 @@ class RockPaperScissors {
          return Math.floor(Math.random() * arr.length)
       }
       return this.variants[getRandom(this.variants)]
+   }
+
+   winner(playerVariantIndex, computerVariantIndex) {
+      const resultMatrix = 
+         [
+            ['draw' , 'win'  , 'loss'],
+            ['loss' , 'draw' , 'win' ],
+            ['win'  , 'loss' , 'draw']
+         ] // outcome of events matrix  🗿 ✂️ 🧻 
+      
+      return resultMatrix[computerVariantIndex][playerVariantIndex]
+   }
+
+   showResult( gameResult ) {
+      const resultElem = this.render.result;
+      resultElem.innerText = gameResult;
+
+      resultElem.style.color = 'LightSlateGray'
+      if ( gameResult === 'win' ) resultElem.style.color = 'SeaGreen'
+      if ( gameResult == 'loss' ) resultElem.style.color = 'Crimson'
+      
+      return resultElem
    }
 }
 
@@ -50,9 +79,13 @@ class Render {
       this.computer.append(this.createElement('div', 'side-title', '', 'Computer'));
 
       this.sides.append(this.computer, this.versus, this.player);
-      
+
+      this.result = this.createElement('section', 'result', '', 'tap any arm to start')
+
+
       this.app.append(this.title);
       this.app.append(this.choiceBlock);
+      this.app.append(this.result);
       this.app.append(this.sides);
       
    }
@@ -67,14 +100,22 @@ class Render {
 
    createStory(variant, side) {
       let emoji = '';
-      if ( variant === 'rock' )  emoji = '✊🏼';
-      if ( variant === 'paper' )  emoji = '✋🏼';
-      if ( variant === 'scissors' )  emoji = '✌🏼';
-      Array.from(side.children).forEach(e => e.classList.remove('history-current'));
+      if ( variant === 'rock' ) emoji = this.rock.textContent;
+      if ( variant === 'paper' ) emoji = this.paper.textContent;
+      if ( variant === 'scissors' ) emoji = this.scissors.textContent;
+
+      const childrenArr = Array.from(side.children)
+      if (childrenArr.length >= 7 ) {
+         console.log('codition true')
+         childrenArr[childrenArr.length - 1].remove()
+         childrenArr.pop()
+      }
+      childrenArr.forEach(e => e.classList.remove('history-current'));
+
       let currentElem = this.createElement('div', 'history', '' , emoji);
       currentElem.classList.add('history-current');
+
       side.firstElementChild.after(currentElem);
-      
    }
 }
 
